e3f0ce6a76566e1b8ba15ee2fcb7d267972acff0
Bananaslama277 <tracesmith555@gmail.com>
2023-01-30 08:46:54 -0600

    Bonus

--- SwiftIfElseExercise.playground/Contents.swift
+++ SwiftIfElseExercise.playground/Contents.swift
@@ -1,4 +1,5 @@
-let turtle = "Donatello"
+let turtle = String()
+
 if (turtle == "Leonardo"){
     print("Katana")
 }else if (turtle == "Donatello"){
@@ -11,3 +12,14 @@ if (turtle == "Leonardo"){
     print("Heros in a half shell. Turtle Power!")
 }
 
+// Bonus
+
+let turtleVillains = String()
+
+if (turtleVillains == "Shredder"){
+    print("Tonight I dine on turtle soup.")
+}else if (turtleVillains == "Krang"){
+    print("I HATE Turtles, I despise Turtles!")
+}else {
+    print("Oh no! It must be the Foot Clan")
+}
