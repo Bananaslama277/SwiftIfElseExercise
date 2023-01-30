adc7262352cf146ac684f0dc7d16a933f5811ce6
Bananaslama277 <tracesmith555@gmail.com>
2023-01-30 08:43:31 -0600

    Initial Commit

---
+++ SwiftIfElseExercise.playground/Contents.swift
@@ -0,0 +1,13 @@
+let turtle = "Donatello"
+if (turtle == "Leonardo"){
+    print("Katana")
+}else if (turtle == "Donatello"){
+        print("Bo Staff")
+}else if (turtle == "Raphael"){
+    print("Twin Sai")
+}else if (turtle == "Michelangelo"){
+    print("Dual Nunchaku")
+}else{
+    print("Heros in a half shell. Turtle Power!")
+}
+
